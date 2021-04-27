e1f125af81ecbfb624b4ef905f826172f793b9b9
alexx19 <alexx198906@gmail.com>
2021-04-26 20:48:43 -0500

    demo entity, repository, controller and unittest

--- src/main/java/com/aurriola/microservice/DemoJenkinsMicroserviceApplication.java
+++ src/main/java/com/aurriola/microservice/DemoJenkinsMicroserviceApplication.java
@@ -1,7 +1,11 @@
 package com.aurriola.microservice;
 
+import com.aurriola.microservice.entity.ThemeParkRide;
+import com.aurriola.microservice.repository.ThemeParkRideRepository;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 public class DemoJenkinsMicroserviceApplication {
@@ -10,4 +14,12 @@ public class DemoJenkinsMicroserviceApplication {
         SpringApplication.run(DemoJenkinsMicroserviceApplication.class, args);
     }
 
+    @Bean
+    public CommandLineRunner sampleData(ThemeParkRideRepository repository) {
+        return (args) -> {
+            repository.save(new ThemeParkRide("Rollercoaster", "Train ride that speeds you along.", 5, 3));
+            repository.save(new ThemeParkRide("Log flume", "Boat ride with plenty of splashes.", 3, 2));
+            repository.save(new ThemeParkRide("Teacups", "Spinning ride in a giant tea-cup.", 2, 4));
+        };
+    }
 }
---
+++ src/main/java/com/aurriola/microservice/controller/ThemeParkRideController.java
@@ -0,0 +1,33 @@
+package com.aurriola.microservice.controller;
+
+import com.aurriola.microservice.entity.ThemeParkRide;
+import com.aurriola.microservice.repository.ThemeParkRideRepository;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.server.ResponseStatusException;
+
+
+@RestController
+public class ThemeParkRideController {
+    private final ThemeParkRideRepository themeParkRideRepository;
+
+    public ThemeParkRideController(ThemeParkRideRepository themeParkRideRepository) {
+        this.themeParkRideRepository = themeParkRideRepository;
+    }
+
+    @GetMapping(value = "/ride", produces = MediaType.APPLICATION_JSON_VALUE)
+    public Iterable<ThemeParkRide> getRides() {
+        return themeParkRideRepository.findAll();
+    }
+
+    @GetMapping(value = "/ride/{id}", produces = MediaType.APPLICATION_JSON_VALUE)
+    public ThemeParkRide getRide(@PathVariable long id){
+        return themeParkRideRepository.findById(id).orElseThrow(() -> new ResponseStatusException(HttpStatus.NOT_FOUND, String.format("Invalid ride id %s", id)));
+    }
+
+    @PostMapping(value = "/ride", consumes = MediaType.APPLICATION_JSON_VALUE, produces = MediaType.APPLICATION_JSON_VALUE)
+    public ThemeParkRide createRide(@RequestBody ThemeParkRide themeParkRide) {
+        return themeParkRideRepository.save(themeParkRide);
+    }
+}
